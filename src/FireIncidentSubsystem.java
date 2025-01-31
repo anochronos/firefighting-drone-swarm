c2482e8b2c9ee1b32f46aca92a28d7ba3aeec046
abdua <Abduallahaldabibi@gmail.com>
2025-01-31 18:47:34 -0500

    Added CSV parser and Ticket class to be sent to the scheduler.
    Missing: sendFireRequest Implementation and The scheduler attribute

--- src/FireIncidentSubsystem.java
+++ src/FireIncidentSubsystem.java
@@ -1,3 +1,48 @@
+// Imports
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ *  Fire Incident subsystem is to read in events using the format shown above:
+ * Time, Zone ID, Event Type and Severity. Each line of input is to be sent to the Scheduler
+ */
 public class FireIncidentSubsystem {
-    
+    /**
+     * Reads the event file into an ArrayList.
+     *
+     * @param filePath Path to the event file.
+     */
+    public void readFile(String filePath) {
+        try (Scanner scanner = new Scanner(new File(filePath))) {
+            if (scanner.hasNextLine()) {
+                scanner.nextLine();
+            }
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] lineData = line.split(",");
+                createFireRequest(lineData);
+                }
+            }
+        catch (FileNotFoundException e) {
+            System.err.println("Event file not found: " + e.getMessage());
+        }
+    }
+    public void createFireRequest(String[] event){
+        FireIncidentTicket eventTicket = new FireIncidentTicket(event[0],event[1],event[2],event[3]);
+        System.out.println(eventTicket);
+    }
+
+    public void sendFireRequest(){
+    }
+
+    /**
+     * Main method to test IF THE CLASS WORKS.
+     */
+    public static void main(String[] args) {
+        FireIncidentSubsystem subsystem = new FireIncidentSubsystem();
+        String eventFilePath = "src/Config/Sample_event_file.csv";
+        subsystem.readFile(eventFilePath);
+
+    }
 }
--- src/FireIncidentTicket.java
+++ src/FireIncidentTicket.java
@@ -1,3 +1,11 @@
-public class FireIncidentTicket {
-    
+
+public record FireIncidentTicket(String timeStamp, String zoneID, String eventType, String severity){
+    @Override
+    public String toString(){
+        return "FireIncidentTicket [timeStamp="+timeStamp+", " +
+                "zoneID="+zoneID+", eventType="+eventType+", severity="+severity+"]";
+
+    }
+
 }
+
