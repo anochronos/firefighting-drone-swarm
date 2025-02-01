1e07f864f42c8b8d6a61a4204862401a371f1186
Akshun <135464796+anochronos@users.noreply.github.com>
2025-01-31 20:06:01 -0500

    Add scheduler and update fire system

--- src/Scheduler.java
+++ src/Scheduler.java
@@ -1,5 +1,62 @@
-public class Scheduler {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+public class Scheduler implements Runnable{
+    private int requestsCompleted = 0;
+    private boolean requestAvailable;
+    private String message;
+    private boolean droneAvailable;
+
+
+    public Scheduler() {
+        this.message = "Fly to fire";
     }
+
+    public int getRequestsCompleted() {
+        return requestsCompleted;
+    }
+
+
+    public synchronized void sendMessage(String message) { // drone
+        while (!droneAvailable) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+            }
+        }
+        System.out.println("Scheduler: Sent message '" + message +"' to available drone");
+        droneAvailable = false;
+        notifyAll();
+    }
+
+    public synchronized void receiveMessage(String message) { // drone
+        while (droneAvailable) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+
+            }
+        }
+        System.out.println("Scheduler: Received message '" + message +"' from drone");
+        droneAvailable = true;
+        requestAvailable = true;
+        requestsCompleted++;
+        notifyAll();
+    }
+
+    public synchronized void receiveRequest(FireIncidentTicket request) { // fire system
+        while (!requestAvailable) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+            }
+        }
+        System.out.println("Scheduler: Received request from the fire incident system");
+        sendMessage(message);
+        notifyAll();
+    }
+    
+    @Override
+    public void run() {
+
+    }
+
 }
